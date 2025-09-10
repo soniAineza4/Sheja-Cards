457a83eddef7385717d83f032f37a1a9e709814e
soniAineza4 <inezasonia124@gmail.com>
2025-09-10 15:34:51 +0200

    ze 1st boilerplate

---
+++ next.config.js
@@ -0,0 +1,29 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8090",
+      },
+    ],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
